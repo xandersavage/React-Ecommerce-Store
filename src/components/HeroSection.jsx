c03181ba04846b885445db7f690afb203ba62a8c
thexandersavage <swankylex@gmail.com>
2024-07-24 11:34:41 +0100

    fixed the overflow problem for the hero section in homepage

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,20 +4,21 @@ import fashionTwo from '../assets/images/hero-2.jpg';
 const HeroSection = () => {
 	return (
 		<>
-			<div className="hero min-h-screen sm:min-w-full prose">
-				<div className="hero-content flex flex-col sm:flex-row lg:flex-row">
+			<div className="hero min-h-screen w-full overflow-hidden prose">
+				<div className="hero-content flex flex-col sm:flex-row lg:flex-row items-center justify-center px-4 sm:px-8 lg:px-16">
 					<img
 						src={fashionTwo}
-						className="max-w-md sm:max-w-sm rounded-lg shadow-2xl"
+						className="max-w-full sm:max-w-sm rounded-lg shadow-2xl"
+						alt="Fashion"
 					/>
-					<div>
-						<h1 className="text-5xl text-center font-bold">
+					<div className="text-center sm:text-left sm:ml-8 lg:ml-16">
+						<h1 className="text-5xl font-bold">
 							Elevate Your Style With Exclusive Collections
 						</h1>
 						<p className="py-6">
 							Discover the latest trends in fashion and enjoy personalized
 							recommendations tailored to your style. Shop now and experience
-							luxury at its finest
+							luxury at its finest.
 						</p>
 						<button className="btn btn-primary rounded-none">
 							Explore Now
@@ -28,4 +29,5 @@ const HeroSection = () => {
 		</>
 	);
 };
+
 export default HeroSection;
