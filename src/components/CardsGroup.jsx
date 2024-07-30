339f35157780ff3d4aa93adbb1471b66f346f088
thexandersavage <swankylex@gmail.com>
2024-07-30 19:29:59 +0100

    added mens clothes images and some fonts

--- src/components/CardsGroup.jsx
+++ src/components/CardsGroup.jsx
@@ -10,7 +10,7 @@ const CardsGroup = () => {
 	return (
 		<section className="py-12">
 			<div className="container mx-auto px-4">
-				<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-6">
+				<div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 gap-6">
 					{mensClothingItems.map((item) => (
 						<ClothingCard
 							key={item.id}
